8103c927dbbd3d6ac3424795bcc4a5c697207291
Carey Metcalfe <carey@cmetcalfe.ca>
2020-12-29 13:55:15 -0500

    Change from package to module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="checkflac",
-      version="0.0.1",
+      version="0.0.2",
       description="Ensure your personal CD rips conform to a certain standard",
       url="https://github.com/pR0Ps/check-flac",
       license="MIT",
@@ -16,7 +16,7 @@ setup(name="checkflac",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
       ],
-      packages=["checkflac"],
+      py_modules=["checkflac"],
       install_requires=["pytaglib >= 1.4.0"],
-      entry_points={'console_scripts': ["check-flac=checkflac.checkflac:main"]}
+      entry_points={'console_scripts': ["check-flac=checkflac:main"]}
 )
